a756e35477b48227ac7b497ef61f1152bb75f733
BENJAMIN <ambluse@163.com>
2018-01-12 16:52:37 +0800

    Update consumer_msg.sql

--- mom-client/sql/consumer_msg.sql
+++ mom-client/sql/consumer_msg.sql
@@ -9,7 +9,7 @@ CREATE TABLE `consumer_msg` (
   `msg_content` mediumtext NOT NULL COMMENT '消息体内容',
   `retry_count` int(2) DEFAULT NULL COMMENT '消费次数',
   `router_key` varchar(20) NOT NULL DEFAULT '' COMMENT '消息队列名称',
-  `status` int(1) NOT NULL  COMMENT '消息状态：100 消息消费初始化,101 消息消费成功,102 消息消费失败',
+  `status` int(3) NOT NULL  COMMENT '消息状态：100 消息消费初始化,101 消息消费成功,102 消息消费失败，103 消息处理中',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`msg_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
